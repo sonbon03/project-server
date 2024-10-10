aeea4b8564d4b1ffdf4f3397027864ec64e9438d
sontt <trinhson.mirailabs@gmail.com>
2024-10-10 17:47:42 +0700

    test-signin-signup

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,40 +2,40 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
-import { StoreEntity } from './entities/store.entity';
 import { AdminEntity } from './entities/admin.entity';
 import { MailService } from '../mail/mail.service';
-import { BadRequestException } from '@nestjs/common';
-import { CreateUserStoreDto } from './dto/create-store-user.dto';
-import { Repository, Timestamp } from 'typeorm';
-import { TypeCurrent } from '../utils/middlewares/current-user.middleware';
-import { Roles } from '../utils/enums/user-roles.enum';
-import { Status } from '../utils/enums/user-status.enum';
-
-const mockUserRepository = {
-  create: jest.fn(),
-  save: jest.fn(),
-  findOne: jest.fn(),
-};
-
-const mockStoreRepository = {
-  create: jest.fn(),
-  save: jest.fn(),
-};
-
-const mockAdminRepository = {
-  findOne: jest.fn(),
-};
-
-const mockMailService = {
-  sendVerificationEmail: jest.fn(),
-};
+import { BadRequestException, HttpException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { SignInDto } from './dto/signin.dto';
+import { AdminDto } from './dto/admin.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { hash } from 'bcrypt';
 
 describe('UsersService', () => {
   let usersService: UsersService;
-  let userRepository: Repository<UserEntity>;
-  let storeRepository: Repository<StoreEntity>;
+  let usersRepository: Repository<UserEntity>;
   let adminRepository: Repository<AdminEntity>;
+  let mailService: MailService;
+
+  const mockUsersRepository = {
+    findOne: jest.fn(),
+    create: jest.fn(),
+    save: jest.fn(),
+    createQueryBuilder: jest.fn().mockReturnThis(),
+    leftJoinAndSelect: jest.fn().mockReturnThis(),
+    where: jest.fn().mockReturnThis(),
+    getOne: jest.fn(),
+  };
+
+  const mockAdminRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn(),
+  };
+
+  const mockMailService = {
+    sendVerificationEmail: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -43,239 +43,124 @@ describe('UsersService', () => {
         UsersService,
         {
           provide: getRepositoryToken(UserEntity),
-          useValue: mockUserRepository,
-        },
-        {
-          provide: getRepositoryToken(StoreEntity),
-          useValue: mockStoreRepository,
+          useValue: mockUsersRepository,
         },
         {
           provide: getRepositoryToken(AdminEntity),
           useValue: mockAdminRepository,
         },
-        { provide: MailService, useValue: mockMailService },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
       ],
     }).compile();
 
     usersService = module.get<UsersService>(UsersService);
-    userRepository = module.get<Repository<UserEntity>>(
+    usersRepository = module.get<Repository<UserEntity>>(
       getRepositoryToken(UserEntity),
-    );
-    storeRepository = module.get<Repository<StoreEntity>>(
-      getRepositoryToken(StoreEntity),
     );
     adminRepository = module.get<Repository<AdminEntity>>(
       getRepositoryToken(AdminEntity),
     );
+    mailService = module.get<MailService>(MailService);
   });
 
-  describe('createModerator', () => {
-    it('should create a moderator successfully', async () => {
-      const createUserStoreDto: CreateUserStoreDto = {
-        user: {
-          email: 'moderator@example.com',
-          password: 'strongpassword',
-          id: '',
-          createdAt: new Date() as unknown as Timestamp,
-          updatedAt: new Date() as unknown as Timestamp,
-          roles: Roles.SUPERADMIN,
-          storeId: '',
-          status: Status.PENDING,
-          store: new StoreEntity(),
-          emailVerificationToken: '',
-          admin: new AdminEntity(),
-        },
-        store: {
-          name: 'Test Store',
-          typeStore: 'Retail', // Thêm loại cửa hàng
-          address: '123 Test St',
-          id: '',
-          user: new UserEntity(),
-          employees: [],
-          warehouse: [],
-          category: [],
-          products: [],
-          promotions: [],
-          orders: [],
-          vouchers: [],
-        },
+  describe('signup', () => {
+    it('should successfully signup a new user', async () => {
+      const adminDto: AdminDto = {
+        email: 'son12@gmail.com',
+        password: '12345',
+        phone: '0123456789',
+        name: 'Admin Name',
       };
 
-      const mockStore: StoreEntity = {
-        id: '0466c77e-6314-4c51-8664-9a4b159237b8',
-        name: 'store 1',
-        typeStore: '1',
-        address: 'asaa',
-        user: null, // Nếu không có user, có thể để null
-        employees: [], // Mảng rỗng cho employees nếu không có
-        warehouse: [], // Mảng rỗng cho warehouse nếu không có
-        category: [], // Mảng rỗng cho category nếu không có
-        products: [], // Mảng rỗng cho products nếu không có
-        promotions: [], // Mảng rỗng cho promotions nếu không có
-        orders: [], // Mảng rỗng cho orders nếu không có
-        vouchers: [], // Mảng rỗng cho vouchers nếu không có
-      } as StoreEntity;
-
-      const currentAdmin: TypeCurrent = {
-        idAdmin: '167f13c4-e46c-47e7-aeac-6089b76c3e86',
-        idUser: '9e986ad5-a098-441d-badc-e92703bf4301',
-        idStore: mockStore,
-        roles: Roles.ADMIN,
-      };
-
-      const mockAdmin: AdminEntity = { id: 'admin-id' } as AdminEntity;
-
-      // Thêm thuộc tính cần thiết cho UserEntity
-      const mockUser: UserEntity = {
-        id: 'user-id',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        roles: ['MODERATOR'], // Hoặc sử dụng enum nếu có
-        email: createUserStoreDto.user.email,
-        password: createUserStoreDto.user.password,
-        // Thêm các thuộc tính khác nếu cần thiết
-      } as unknown as UserEntity;
-
-      mockAdminRepository.findOne.mockResolvedValue(mockAdmin);
-      mockStoreRepository.create.mockReturnValue(createUserStoreDto.store);
-      mockStoreRepository.save.mockResolvedValue(createUserStoreDto.store);
-      mockUserRepository.create.mockReturnValue(mockUser);
-      mockUserRepository.save.mockResolvedValue(mockUser);
-
-      const result = await usersService.createModerator(
-        createUserStoreDto,
-        currentAdmin,
-      );
-
-      expect(result).toEqual(mockUser);
-      expect(mockStoreRepository.create).toHaveBeenCalledWith(
-        createUserStoreDto.store,
-      );
-      expect(mockUserRepository.create).toHaveBeenCalledWith(
-        createUserStoreDto.user,
-      );
-    });
-
-    it('should throw BadRequestException if email already exists', async () => {
-      const createUserStoreDto: CreateUserStoreDto = {
-        user: {
-          email: 'moderator@example.com',
-          password: 'strongpassword',
-          createdAt: new Date() as unknown as Timestamp, // Thêm ngày tạo
-          updatedAt: new Date() as unknown as Timestamp, // Thêm ngày cập nhật
-          roles: Roles.ADMIN,
-          id: '',
-          storeId: '',
-          status: Status.ACTIVE,
-          store: new StoreEntity(),
-          emailVerificationToken: '',
-          admin: new AdminEntity(),
-        },
-        store: {
-          name: 'Test Store',
-          typeStore: 'Retail',
-          address: '123 Test St',
-          id: '0466c77e-6314-4c51-8664-9a4b159237b8',
-          user: new UserEntity(),
-          employees: [],
-          warehouse: [],
-          category: [],
-          products: [],
-          promotions: [],
-          orders: [],
-          vouchers: [],
-        },
-      };
-
-      const mockStore: StoreEntity = {
-        id: '0466c77e-6314-4c51-8664-9a4b159237b8',
-        name: 'store 1',
-        typeStore: '1',
-        address: 'asaa',
-        user: null, // Nếu không có user, có thể để null
-        employees: [], // Mảng rỗng cho employees nếu không có
-        warehouse: [], // Mảng rỗng cho warehouse nếu không có
-        category: [], // Mảng rỗng cho category nếu không có
-        products: [], // Mảng rỗng cho products nếu không có
-        promotions: [], // Mảng rỗng cho promotions nếu không có
-        orders: [], // Mảng rỗng cho orders nếu không có
-        vouchers: [], // Mảng rỗng cho vouchers nếu không có
-      } as StoreEntity;
-
-      const currentAdmin: TypeCurrent = {
-        idAdmin: '167f13c4-e46c-47e7-aeac-6089b76c3e86',
-        idUser: '9e986ad5-a098-441d-badc-e92703bf4301',
-        idStore: mockStore,
-        roles: Roles.ADMIN,
-      };
-
-      // Giả lập rằng email đã tồn tại
-      mockUserRepository.findOne.mockResolvedValue(createUserStoreDto.user);
-
-      await expect(
-        usersService.createModerator(createUserStoreDto, currentAdmin),
-      ).rejects.toThrow(BadRequestException);
-      expect(mockUserRepository.findOne).toHaveBeenCalledWith({
-        where: { email: createUserStoreDto.user.email },
+      mockUsersRepository.findOne.mockResolvedValue(null); // No user exists
+      mockAdminRepository.create.mockReturnValue({
+        id: '167f13c4-e46c-47e7-aeac-6089b76c3e86',
       });
+      mockAdminRepository.save.mockResolvedValue({
+        id: '167f13c4-e46c-47e7-aeac-6089b76c3e86',
+      });
+      mockUsersRepository.create.mockReturnValue({ email: adminDto.email });
+      mockUsersRepository.save.mockResolvedValue({
+        id: '9e986ad5-a098-441d-badc-e92703bf4301',
+        email: adminDto.email,
+        password: adminDto.password,
+        emailVerificationToken: uuidv4(),
+      });
+      mockMailService.sendVerificationEmail.mockResolvedValue(null);
+
+      const result = await usersService.signup(adminDto);
+
+      expect(result).toHaveProperty('email', adminDto.email);
+      expect(mockMailService.sendVerificationEmail).toHaveBeenCalled();
     });
 
-    it('should throw BadRequestException if store name contains special characters', async () => {
-      const createUserStoreDto: CreateUserStoreDto = {
-        user: {
-          email: 'moderator@example.com',
-          password: 'strongpassword',
-          createdAt: new Date() as unknown as Timestamp, // Thêm ngày tạo
-          updatedAt: new Date() as unknown as Timestamp, // Thêm ngày cập nhật
-          roles: Roles.ADMIN,
-          id: '',
-          storeId: '',
-          status: Status.ACTIVE,
-          store: new StoreEntity(),
-          emailVerificationToken: '',
-          admin: new AdminEntity(),
-        },
-        store: {
-          name: 'Test Store',
-          typeStore: 'Retail',
-          address: '123 Test St',
-          id: '0466c77e-6314-4c51-8664-9a4b159237b8',
-          user: new UserEntity(),
-          employees: [],
-          warehouse: [],
-          category: [],
-          products: [],
-          promotions: [],
-          orders: [],
-          vouchers: [],
-        },
+    it('should throw an error if email already exists', async () => {
+      const adminDto: AdminDto = {
+        email: 'son@gmail.com',
+        password: '12345',
+        phone: '0123456789',
+        name: 'Admin Name',
       };
 
-      const mockStore: StoreEntity = {
-        id: '0466c77e-6314-4c51-8664-9a4b159237b8',
-        name: 'store 1',
-        typeStore: '1',
-        address: 'asaa',
-        user: null, // Nếu không có user, có thể để null
-        employees: [], // Mảng rỗng cho employees nếu không có
-        warehouse: [], // Mảng rỗng cho warehouse nếu không có
-        category: [], // Mảng rỗng cho category nếu không có
-        products: [], // Mảng rỗng cho products nếu không có
-        promotions: [], // Mảng rỗng cho promotions nếu không có
-        orders: [], // Mảng rỗng cho orders nếu không có
-        vouchers: [], // Mảng rỗng cho vouchers nếu không có
-      } as StoreEntity;
+      mockUsersRepository.findOne.mockResolvedValue({}); // User exists
 
-      const currentAdmin: TypeCurrent = {
-        idAdmin: '167f13c4-e46c-47e7-aeac-6089b76c3e86',
-        idUser: '9e986ad5-a098-441d-badc-e92703bf4301',
-        idStore: mockStore,
-        roles: Roles.ADMIN,
+      await expect(usersService.signup(adminDto)).rejects.toThrow(
+        BadRequestException,
+      );
+    });
+  });
+
+  describe('signin', () => {
+    it('should successfully signin a user', async () => {
+      const signInDto: SignInDto = {
+        email: 'son@gmail.com',
+        password: '12345',
       };
 
-      await expect(
-        usersService.createModerator(createUserStoreDto, currentAdmin),
-      ).rejects.toThrow(BadRequestException);
+      mockUsersRepository.createQueryBuilder().getOne.mockResolvedValue({
+        id: '9e986ad5-a098-441d-badc-e92703bf4301',
+        email: signInDto.email,
+        password: await hash(signInDto.password, 10), // Mock hashed password
+        store: null,
+      });
+      mockAdminRepository.findOne.mockResolvedValue({ name: 'Admin Name' });
+
+      const result = await usersService.signin(signInDto);
+
+      expect(result).toHaveProperty('email', signInDto.email);
+    });
+
+    it('should throw an error if user does not exist', async () => {
+      const signInDto: SignInDto = {
+        email: 'test@example.com',
+        password: 'password123',
+      };
+
+      mockUsersRepository.createQueryBuilder().getOne.mockResolvedValue(null); // User does not exist
+
+      await expect(usersService.signin(signInDto)).rejects.toThrow(
+        HttpException,
+      );
+    });
+
+    it('should throw an error if password is incorrect', async () => {
+      const signInDto: SignInDto = {
+        email: 'son@gmail.com',
+        password: 'asdasd',
+      };
+
+      mockUsersRepository.createQueryBuilder().getOne.mockResolvedValue({
+        id: '9e986ad5-a098-441d-badc-e92703bf4301',
+        email: signInDto.email,
+        password: await hash('correctpassword', 10), // Mock hashed password
+      });
+
+      await expect(usersService.signin(signInDto)).rejects.toThrow(
+        HttpException,
+      );
     });
   });
 });
